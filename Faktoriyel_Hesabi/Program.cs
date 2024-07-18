9e9d67230fab46a7bc565d4903a27444b81aec4c
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 19:06:18 +0300

    Proje dosyası ekle.

---
+++ Faktoriyel_Hesabi/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Faktoriyel_Hesabi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5; // FAKTÖRİYEL DEĞERİNİ YAZIP SONUÇ ALABİLİRSİN.
+
+            Console.WriteLine("{0}! = {1}", n, Matematik.Faktoriyel(n));
+        }
+    }
+}
